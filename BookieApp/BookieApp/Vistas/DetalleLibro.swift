8ed4bdc18970a48bfb6fbdc6086ade4e8f27d9e4
JoseJavier21 <javier.rojo@escuelaestech.es>
2024-03-14 16:54:39 +0100

    Vista detalla libro y vista generos

--- BookieApp/BookieApp/Vistas/DetalleLibro.swift
+++ BookieApp/BookieApp/Vistas/DetalleLibro.swift
@@ -24,16 +24,89 @@ struct DetalleLibro: View {
                             .padding(.leading, 70)
                             .padding(.trailing, 10)
                     }
+                    .padding(.top, 15)
                     
-                    Image(systemName: "globe")
-                        .frame(width: 166, height: 196)
-                        .foregroundColor(.blue)
-                        .background(Color.gray, in: .rect)
-                        .cornerRadius(20)
+                    HStack{
+                        Image(systemName: "globe")
+                            .frame(width: 207, height: 251)
+                            .background(Color.gray, in: .rect)
+                            .cornerRadius(20)
+                        
+                        Image(systemName: "heart")
+                            .imageScale(.large)
+                            .padding(.top, 220)
+                            .padding(.trailing, 70)
+                            .frame(width: 50, height: 50)
+                            .padding(.leading, 20)
+                    }
+                    .padding(.leading, 75)
+                    .padding(.top, 20)
+                    
+                    Text("Nº de Paginas")
+                        .padding(.leading, 250)
+                        .padding(.top, 20)
+                        .bold()
+                    Text("259")
+                        .padding(.leading, 250)
+                    
+                    Text("Titulo")
+                        .font(.title3)
+                        .padding(.trailing, 300)
+                        .bold()
+                    Text("EL conde de no se que")
+                        .padding(.trailing, 170)
+                    
+                    Text("Autor")
+                        .font(.title3)
+                        .padding(.trailing, 300)
+                        .padding(.top, 3)
+                        .bold()
+                    Text("Akexandre Duque")
+                        .padding(.leading, 30)
+                        .padding(.trailing, 220)
+                        
+                    Text("Género")
+                        .font(.title3)
+                        .padding(.top, 3)
+                        .padding(.trailing, 285)
+                        .bold()
+                    Text("Aventura")
+                        .padding(.trailing, 270)
                     
                     
+                    Divider()
                     
-                    Spacer()
+                    Text("Filtros")
+                        .font(.title3)
+                        .padding(.trailing, 290)
+                        .bold()
+
+                    
+                    HStack{
+                        ViewFiltros()
+                            .padding([.leading, .trailing], 10)
+                        ViewFiltros()
+                            .padding([.leading, .trailing], 10)
+                        ViewFiltros()
+                            .padding([.leading, .trailing], 10)
+                        ViewFiltros()
+                            .padding([.leading, .trailing], 10)
+                    }
+                    
+                    Text("Editorial")
+                        .font(.title3)
+                        .padding(.trailing, 270)
+                        .padding(.top, 30)
+                        .bold()
+                    Text("una editorial")
+                        .padding(.trailing, 250)
+                    
+                    HStack{
+                        Button("Intercambio"){}
+                        Button("Escribir reseña"){}
+                            .padding(.leading, 40)
+                    }
+                    .padding(.top, 20)
                 }
                 
             }
