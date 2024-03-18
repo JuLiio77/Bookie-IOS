cb1ccd892bf344d0d9a90e3f95b28786ee0b182f
JuLiio77 <92015093+JuLiio77@users.noreply.github.com>
2024-03-18 16:14:08 +0100

    perfil subido

--- BookieApp/BookieApp/Vistas/ViewPerfil/PerfilView.swift
+++ BookieApp/BookieApp/Vistas/ViewPerfil/PerfilView.swift
@@ -8,10 +8,89 @@
 import SwiftUI
 
 struct PerfilView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+            VStack {
+
+                Text("Julio Vera")
+
+                    .font(.title)
+                    .padding()
+                
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+
+                VStack{
+                    Text("Linares")
+                        .padding(.horizontal)
+
+                    Text("4,8 ★")
+
+                }
+
+                .padding()
+
+                HStack {
+
+                    Image(systemName: "book.circle.fill")
+                    Text("Misterio")
+                    Spacer()
+                    
+                    Image(systemName: "book.circle.fill")
+
+                    Text("Aventura")
+                    Spacer()
+
+                    Image(systemName: "book.circle.fill")
+                    Text("Filosofía")
+
+                }
+
+                .padding()
+                Spacer()
+
+            }
+
+            .background(
+            LinearGradient(gradient: Gradient(colors: [Color.white, Color.button]), startPoint: .top, endPoint: .bottom)
+
+            )
+
+        }
+
     }
-}
+
+//    struct BookItemView: View {
+//
+//        let title: String
+//
+//        let author: String
+//
+//        var body: some View {
+//    HStack {
+//
+//                Image(systemName: "book.fill")
+//
+//                VStack(alignment: .leading) {
+//
+//                    Text(title)
+//              .font(.headline)
+//
+//                    Text(author)
+//
+//                        .font(.subheadline)
+//
+//                }
+//
+//            }
+//
+//        }
+//
+//    }
+
 
 #Preview {
     PerfilView()
