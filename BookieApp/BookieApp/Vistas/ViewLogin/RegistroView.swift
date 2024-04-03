261db5ec3f64fef354c2cc2410cebb9ebce110ae
Levexm <156109384+Levexm@users.noreply.github.com>
2024-04-03 18:23:03 +0200

    recup. modificado

--- BookieApp/BookieApp/Vistas/ViewLogin/RegistroView.swift
+++ BookieApp/BookieApp/Vistas/ViewLogin/RegistroView.swift
@@ -9,113 +9,106 @@ import SwiftUI
 
 struct RegistroView: View {
     
-    //@EnvironmentObject var userData: FuncionLogin
+    /*@EnvironmentObject var userData: FuncionLo*/
     
     @State var mostrarContrasenia: Bool = false
     @State var mostrarContrasenia1: Bool = false
-
-
+    
+    
     var body: some View {
         VStack {
             
-            Text("Registro")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold()
-                .padding()
-            
-            
-            //TextField("Nombre", text: $userData.name)
-                .bold()
-                .padding()
-                .background(Color.color)
-                .cornerRadius(30)
-                .padding(.top,90)
-            
-            //TextField("Correo electronico", text: $userData.email)
-                .bold()
-                .padding()
-                .background(Color.color)
-                .cornerRadius(30)
-                .padding(.top,40)
-            
-            
-            ZStack(alignment: .trailingFirstTextBaseline) {
-                if mostrarContrasenia {
-                    //TextField("Contraseña", text: $userData.password)
-                    //.padding()
-                    //.background(Color.color)
-                    //.cornerRadius(30)
-                    //.padding(.top, 40)
-                    //
-                } else {
-                    // SecureField("Contraseña", text: $userData.password)
-                    //                        .padding()
-                    //                        .background(Color.color)
-                    //                        .cornerRadius(30)
-                    //                        .padding(.top, 40)
-                }
-                
-                Button(action: {
-                    mostrarContrasenia.toggle()
-                }) {
-                    Image(systemName: mostrarContrasenia ? "eye.fill" : "eye.slash.fill")
-                        .foregroundColor(.primary)
-                        .padding(10)
-                }
-                .padding(.trailing, 15)
-            }
-            
-            
-            ZStack(alignment: .trailingFirstTextBaseline) {
-                if mostrarContrasenia1 {
-                    //                    //TextField("Contraseña", text: $userData.repassword)
-                    //                        .padding()
-                    //                        .background(Color.color)
-                    //                        .cornerRadius(30)
-                    //                        .padding(.top, 40)
-                    //
-                } else {
-                    //                  //  SecureField("Contraseña", text: $userData.repassword)'
-                    //                        .padding()
-                    //                        .background(Color.color)
-                    //                        .cornerRadius(30)
-                    //                        .padding(.top, 40)
-                    //                }
-                    
-                    Button(action: {
-                        mostrarContrasenia1.toggle()
-                    }) {
-                        Image(systemName: mostrarContrasenia1 ? "eye.fill" : "eye.slash.fill")
-                            .foregroundColor(.primary)
-                            .padding(10)
-                    }
-                    .padding(.trailing, 15)
-                }
-                
-                
-                
-                
-                
-                Button("Siguiente"){
-                    
-                    
-                }
-                .padding(20)
-                .padding(.horizontal, 30)
-                .background(Color.button)
-                .foregroundColor(.black)
-                .cornerRadius(20)
-                .padding([.leading, .trailing], 10)
-                .padding(.top, 75)
-                
-                
-                
-            }
-            .padding()
+            //            Text("Registro")
+            //                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            //                .bold()
+            //                .padding()
+            //
+            //
+            //            TextField("Nombre", text: $userData.name)
+            //                .bold()
+            //                .padding()
+            //                .background(Color.color)
+            //                .cornerRadius(30)
+            //                .padding(.top,90)
+            //
+            //            TextField("Correo electronico", text: $userData.email)
+            //                .bold()
+            //                .padding()
+            //                .background(Color.color)
+            //                .cornerRadius(30)
+            //                .padding(.top,40)
+            //
+            //
+            //            ZStack(alignment: .trailingFirstTextBaseline) {
+            //                if mostrarContrasenia {
+            //                    TextField("Contraseña", text: $userData.password)
+            //                        .padding()
+            //                        .background(Color.color)
+            //                        .cornerRadius(30)
+            //                        .padding(.top, 40)
+            //
+            //                } else {
+            //                    SecureField("Contraseña", text: $userData.password)
+            //                        .padding()
+            //                        .background(Color.color)
+            //                        .cornerRadius(30)
+            //                        .padding(.top, 40)
+            //                }
+            //
+            //                Button(action: {
+            //                    mostrarContrasenia.toggle()
+            //                }) {
+            //                    Image(systemName: mostrarContrasenia ? "eye.fill" : "eye.slash.fill")
+            //                        .foregroundColor(.primary)
+            //                        .padding(10)
+            //                }
+            //                .padding(.trailing, 15)
+            //            }
+            //
+            //            ZStack(alignment: .trailingFirstTextBaseline) {
+            //                if mostrarContrasenia1 {
+            //                    TextField("Contraseña", text: $userData.repassword)
+            //                        .padding()
+            //                        .background(Color.color)
+            //                        .cornerRadius(30)
+            //                        .padding(.top, 40)
+            //
+            //                } else {
+            //                    SecureField("Contraseña", text: $userData.repassword)'
+            //                        .padding()
+            //                        .background(Color.color)
+            //                        .cornerRadius(30)
+            //                        .padding(.top, 40)
+            //                }
+            //
+            //                Button(action: {
+            //                    mostrarContrasenia1.toggle()
+            //                }) {
+            //                    Image(systemName: mostrarContrasenia1 ? "eye.fill" : "eye.slash.fill")
+            //                        .foregroundColor(.primary)
+            //                        .padding(10)
+            //                }
+            //                .padding(.trailing, 15)
+            //            }
+            //
+            //            Button("Siguiente"){
+            //
+            //
+            //            }
+            //            .padding(20)
+            //            .padding(.horizontal, 30)
+            //            .background(Color.button)
+            //            .foregroundColor(.black)
+            //            .cornerRadius(20)
+            //            .padding([.leading, .trailing], 10)
+            //            .padding(.top, 75)
+            //        }
+            //        .padding()
         }
     }
 }
+
 #Preview {
     RegistroView()
-        //.environmentObject(FuncionLogin())
+//        .environmentObject(FuncionLogin())
 }
