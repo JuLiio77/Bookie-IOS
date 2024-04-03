f2c3ccc0ac23c73b4b5d756172570e5e41e7ab37
JuLiio77 <92015093+JuLiio77@users.noreply.github.com>
2024-04-03 18:25:39 +0200

    s

--- BookieApp/BookieApp/Vistas/ViewLogin/FuncionLogin.swift
+++ BookieApp/BookieApp/Vistas/ViewLogin/FuncionLogin.swift
@@ -7,8 +7,24 @@
 
 import SwiftUI
 
-struct FuncionLogin{
+class FuncionLogin : ObservableObject{
     
+    @Published var name: String = ""
+    @Published var password: String = ""
+    @Published var repassword: String = ""
+    @Published var email: String = ""
+    
+    func check(){
+        if password == repassword{
+            print("Contraseña igual")
+        }else{
+            print("Contraseña diferente")
+        }
+    }
+    
+    func register(){
+        print("Registrado")
+    }
     
     
 
