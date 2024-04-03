5e1b369b15dd1fda74749c3f079a1a14b5bdb3ad
JoseJavier21 <javier.rojo@escuelaestech.es>
2024-04-03 17:21:57 +0200

    funcionalidad reg

--- BookieApp/BookieApp/Vistas/ViewLogin/FuncionLogin.swift
+++ BookieApp/BookieApp/Vistas/ViewLogin/FuncionLogin.swift
@@ -7,11 +7,28 @@
 
 import SwiftUI
 
-struct FuncionLogin{
+class FuncionLogin: ObservableObject{
+    
+    @Published var name: String = ""
+    @Published var password: String = ""
+    @Published var repassword: String = ""
+    @Published var email: String = ""
     
     
+    func check(){
+        
+        if password == repassword{
+            print("contraseña igual")
+        }else{
+            print("contraseña diferente")
+        }
+        
+    }
     
-
+    func register(){
+        
+     print("registrado")
+    }
 }
 
 
