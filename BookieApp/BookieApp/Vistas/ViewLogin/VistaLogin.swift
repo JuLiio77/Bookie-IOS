df852f5e55b4853df4f5ceffd57df8d37d700a60
JoseJavier21 <javier.rojo@escuelaestech.es>
2024-05-06 18:59:01 +0200

    vista login

--- BookieApp/BookieApp/Vistas/ViewLogin/VistaLogin.swift
+++ BookieApp/BookieApp/Vistas/ViewLogin/VistaLogin.swift
@@ -62,7 +62,7 @@ struct VistaLogin: View {
             
             HStack {
                 
-                Toggle("",isOn: $toggle)
+                Toggle("", isOn: $toggle)
                 Text("Recordar contraseña")
                 
             }
@@ -82,7 +82,7 @@ struct VistaLogin: View {
             Button("Inicio Sesion", action: {
                 datos.check(username: "inigo12314", password: "12345")
                 
-                datos.user()
+                $datos.user
             })
             .padding(20)
             .padding(.horizontal, 30)
