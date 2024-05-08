59b31dd1785884070271526aa21b0ee57ea7c459
JoseJavier21 <javier.rojo@escuelaestech.es>
2024-05-08 17:50:44 +0200

    intento de eliminacion de errores

--- BookieApp/BookieApp/Vistas/ViewFavoritos/LibrosFavoritos.swift
+++ BookieApp/BookieApp/Vistas/ViewFavoritos/LibrosFavoritos.swift
@@ -7,12 +7,8 @@
 
 import SwiftUI
 
-struct LibrosFavoritos: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+class LibrosFavoritos: ObservableObject {
+    
+    
 }
 
-#Preview {
-    LibrosFavoritos()
-}
