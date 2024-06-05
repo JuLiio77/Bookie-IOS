1d013d462704c79dfaab19a06067f70a1f9ae918
JuLiio77 <92015093+JuLiio77@users.noreply.github.com>
2024-06-05 19:31:47 +0200

    hacer pruebas chat

--- BookieApp/BookieApp/Vistas/Chats/ChatView.swift
+++ BookieApp/BookieApp/Vistas/Chats/ChatView.swift
@@ -12,28 +12,32 @@ struct ChatView: View {
 //        Message(mensaje: "Hola", enviarMensajeUsuario: false),
 //        Message(mensaje: "Adios", enviarMensajeUsuario: true)
     ]
+    @State private var responseIndex: Int = 0 // Índice para rastrear la respuesta automática
+
     @State private var nuevoMensaje: String = ""
+    let automaticResponses = ["Hola!", "si claro! Cuentame", "lo siento no esta dispinible", "Hasta luego"] // Respuestas automáticas
+
     
     var body: some View {
         VStack {
             ScrollView {
-                // el srcollreader sirve para desplazarse automaticamente al enviar un nuevo mensaje
-                ScrollViewReader { scrollView in
-                    ForEach(messages) { message in
-                        MessageRow(message: message)
-                            .id(message.id)
-                    }
-                    .onChange(of: messages) { _ in
-                        if let ultimoMensaje = messages.last {
-                            withAnimation {
-                                scrollView.scrollTo(ultimoMensaje.id, anchor: .bottom)
-                            }
-                        }
-                        saveMessages(messages) // Guardar mensajes en UserDefaults cuando cambian
-
-                    }
-                }
-            }
+                         // ScrollViewReader permite desplazarse automáticamente al nuevo mensaje
+                         ScrollViewReader { scrollView in
+                             ForEach(messages) { message in
+                                 MessageRow(message: message)
+                                     .id(message.id) // Asigna un ID a cada fila de mensaje
+                             }
+                             .onChange(of: messages) { _ in
+                                 // Desplazarse automáticamente al último mensaje cuando cambia la lista de mensajes
+                                 if let lastMessage = messages.last {
+                                     withAnimation {
+                                         scrollView.scrollTo(lastMessage.id, anchor: .bottom)
+                                     }
+                                 }
+                                 saveMessages(messages) // Guardar mensajes en UserDefaults cuando cambian
+                             }
+                         }
+                     }
             .background(Color(UIColor.systemGroupedBackground))
             
             HStack {
@@ -57,12 +61,21 @@ struct ChatView: View {
     
     
      // Función para enviar un nuevo mensaje
-     func sendMessage() {
-         guard !nuevoMensaje.isEmpty else { return } // Verifica que el mensaje no esté vacío
-         let newMessage = Message(mensaje: nuevoMensaje, enviarMensajeUsuario: true)
-         messages.append(newMessage) // Añade el nuevo mensaje a la lista
-         nuevoMensaje = "" // Limpia el campo de texto
-     }
+    func sendMessage() {
+        guard !nuevoMensaje.isEmpty else { return } // Verifica que el mensaje no esté vacío
+        let newMessage = Message(mensaje: nuevoMensaje, enviarMensajeUsuario: true)
+        messages.append(newMessage) // Añade el nuevo mensaje a la lista
+        nuevoMensaje = "" // Limpia el campo de texto
+        
+        // Añadir una respuesta automática después de un pequeño retraso
+          DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+              let responseText = self.automaticResponses[self.responseIndex % self.automaticResponses.count]
+              let responseMessage = Message(mensaje: responseText, enviarMensajeUsuario: false)
+              messages.append(responseMessage)
+              responseIndex += 1 // Incrementar el índice para la próxima respuesta automática
+          }
+      }
+    
      
      // Función para cargar los mensajes desde UserDefaults
      func loadMessages() {
